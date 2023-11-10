97ddace2c9e20e1526ba4e561cd51dafef6d502c
Nawaz393 <mohdnawaz6393@gmail.com>
2023-11-10 09:11:27 +0500

    added prototype

--- src/GOF/Lab7/Protoype/ShapeCache.java
+++ src/GOF/Lab7/Protoype/ShapeCache.java
@@ -11,11 +11,11 @@ public class ShapeCache {
       return (Shape) cachedShape.clone();
    }
 
-   // for each shape run database query and create shape
-   // shapeMap.put(shapeKey, shape);
-   // for example, we are adding three shapes
+ 
    
    public static void loadCache() {
+
+      
       Circle circle = new Circle();
       circle.setId("1");
       shapeMap.put(circle.getId(),circle);
--- src/GOF/Lab7/Protoype/Square.java
+++ src/GOF/Lab7/Protoype/Square.java
@@ -2,12 +2,31 @@ package GOF.Lab7.Protoype;
 
 public class Square extends Shape {
 
-   public Square(){
-     type = "Square";
+   public Square(Shape target) {
+      super(target);
+      type = "Square";
    }
 
    @Override
    public void draw() {
       System.out.println("Inside Square::draw() method.");
+   }
+
+   @Override
+   public Object clone() {
+      return new Square(this);
+   }
+
+   
+
+   public boolean equals(Object object) {
+
+      if (!super.equals(object) && (object instanceof Square))
+         return false;
+
+      Shape shape = (Shape) object;
+
+      return this.type == shape.type;
+
    }
 }
