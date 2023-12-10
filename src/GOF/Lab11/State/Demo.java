a5bf8dcfb91e7768d0b51d37f1bf561c0e37554a
Nawaz393 <mohdnawaz6393@gmail.com>
2023-12-10 18:21:06 +0500

    added state

---
+++ src/GOF/Lab11/State/Demo.java
@@ -0,0 +1,9 @@
+package GOF.Lab11.State;
+
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
